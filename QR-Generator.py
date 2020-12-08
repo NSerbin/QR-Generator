aef7ccb55ddbaa4b28d0d8a6f62bda5e26058ee7
Nicolás Serbin <55348850+NSerbin@users.noreply.github.com>
2020-12-08 16:18:31 -0300

    Add files via upload

---
+++ QR-Generator.py
@@ -0,0 +1,16 @@
+import pyqrcode
+import png
+
+def QRGenerator():
+    """ Function to make QR Code """
+    
+    # Insert The web adress 
+    url = input("Enter the web adress: ")
+    # Name of QR code png file
+    name = input("Enter image name to save: ")+ ".png"
+    # Creating QR code
+    qr = pyqrcode.create(url)
+    # Saving QR code as a png file
+    qr.png(name, scale =6)
+
+QRGenerator()
